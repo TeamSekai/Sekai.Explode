913988107c96fa6cf390a154e54c1bdf2612786a
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-09-27 22:58:48 +0900

    おみくじ

---
+++ commands/omikuzi.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+function GenNum() {
+	const min = 1;
+	const max = 5;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('おみくじ')
+		.setDescription('何が出るかな'),
+	execute: async function(interaction) {
+		let number = GenNum();
+		if (number == 1) {
+			await interaction.reply(`凶`);
+		}
+		if (number == 2) {
+			await interaction.reply(`吉`);
+		}
+		if (number == 3) {
+			await interaction.reply(`小吉`);
+		}
+		if (number == 4) {
+			await interaction.reply(`中吉`);
+		}
+		if (number == 5) {
+			await interaction.reply(`大吉`);
+		}
+		else {
+			await interaction.reply(`エラー(function result: ${number}) - ボットが燃えました`);
+		}
+	},
+};
