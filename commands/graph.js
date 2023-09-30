bd2dfa97655e97509f9ae6da964805bcadfe9a4f
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-09-30 20:50:32 +0900

    graph

--- commands/graph.js
+++ commands/graph.js
@@ -1,31 +1,82 @@
 const { ChartJSNodeCanvas } = require("chartjs-node-canvas")
-const { createCanvas, loadImage } = require('canvas');
+const { createCanvas } = require('canvas');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageAttachment } = require('discord.js');
 const fs = require('fs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('graph')
-		.setDescription('test'),
+		.setDescription('グラフを描画します。')
+		.addStringOption(option =>
+			option
+				.setName("value")
+				.setDescription("値を入力(それぞれの値は1, 2, 3のようにコンマで区切ること。)")
+				.setRequired(true)
+		)
+		.addStringOption(option =>
+			option
+				.setName("label")
+				.setDescription("値のラベル(例: 個数) (未入力の場合はvalueとなります)")
+				.setRequired(false)
+		)
+		.addStringOption(option =>
+			option
+				.setName('title')
+				.setDescription(`タイトルを入力(未入力の場合はuser's graphとなります`)
+				.setRequired(false)
+		)
+		.addBooleanOption(option =>
+			option
+				.setName('beginAtZero')
+				.setDescription('グラフの最小値を0に固定するか(デフォルト: false)')
+				.setRequired(false) // 任意のオプション
+		),
 	execute: async function(interaction) {
+
+		const executorID = interaction.user.id;
+		let graphtitle = `${executorID}'s graph`
+		if (interaction.option.getString('title')) {
+			let graphtitle = interaction.option.getString('title');
+		}
+
+		let label = `value`
+		if (interaction.option.getString('label')) {
+			let label = interaction.option.getString('label');
+		}
+
+		let AtZero = false;
+		if (interaction.option.getBoolean('beginAtZero')) {
+			let AtZero = interaction.option.getBoolean('beginAtZero')
+		}
+
+		const valuesString = interaction.options.getString('values');
+		const values = valuesString.split(',').map(val => parseInt(val.trim()));
+
 		const width = 800;
 		const height = 400;
 
+		const data = values
 		const canvas = createCanvas(width, height);
 		const ctx = canvas.getContext('2d');
     	const configuration = {
 			type: 'line',
 			data: {
-				labels: ['1', '2', '3', '4', '5'],
+				labels: values.map((_, index) => `Value ${index + 1}`),
 				datasets: [{
-					label: 'かいるん',
-					data: [12, 19, 3, 0, 10],
+					label: label,
+					data: data,
 					fill: false,
 					borderColor: 'rgb(75, 192, 192)',
 					tension: 0.1,
 				}],
-			}
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: AtZero, // 最小値を0に設定
+					},
+				},
+			},
 		}
 		const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height, backgroundColour:"#01010e" });
 		const orimoto = await chartJSNodeCanvas.renderToBuffer(configuration);
@@ -35,7 +86,7 @@ module.exports = {
 			  name:"chart.png"
 			}],
 			embeds:[{
-			  title: "Chart",
+			  title: graphtitle,
 			  image: {
 				 "url": "attachment://chart.png"
 			  }
