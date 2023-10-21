c1fba6d49a83b0438a8c70f7d67f9c2ff05cbd69
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-21 21:00:33 +0900

    敗北

--- commands/upload-private.js
+++ commands/upload-private.js
@@ -4,9 +4,9 @@ const FormData = require("form-data");
 const config = require("../config.json");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('upload')
-        .setDescription('ファイルをcdnにアップロード')
+	data: new SlashCommandBuilder()
+        .setName('private_upload')
+        .setDescription('(プライベート)ファイルをcdnにアップロード')
         .addAttachmentOption(option =>
             option
                 .setName("file")
@@ -36,9 +36,9 @@ module.exports = {
             const form = new FormData()
             let filename = interaction.options.get("filename")?.value;
             form.append('file', res.data, filename || file.name)
-            let res2 = await axios.post(config.cdnUploadURL, form, { headers: form.getHeaders() });
+            let res2 = await axios.post(config.cdnUploadURL2, form, { headers: form.getHeaders() });
             if (!res2.data?.success) throw new Error();
-            interaction.editReply(`/${res2.data.fileName} としてアップロードしました！`);
+            interaction.editReply(`/private/${res2.data.fileName} としてアップロードしました！`);
 			const userId = interaction.user.id
 			const dmChannel = await userId.createDM();
 			dmChannel.send({
@@ -47,21 +47,10 @@ module.exports = {
 					color: 0x5865f2,
 					fields: [{
 						name: "URL",
-						value: "```" + `https://cdn.mcsv.life/${res2.data.fileName}` + "```",
+						value: "```" + `https://cdn.mcsv.life/private/${res2.data.fileName}` + "```",
 					}]
 				}]
 			});
-            try {
-                if (!config.cfZone || !config.cfToken || !config.cfPurgeUrl) return;
-                axios.post(`https://api.cloudflare.com/client/v4/zones/${config.cfZone}/purge_cache`, {
-                    files: [config.cfPurgeUrl]
-                }, {
-                    headers: {
-                        Authorization: `Bearer ${config.cfToken}`,
-                        'Content-Type': 'application/json'
-                    }
-                })
-            } catch (e) { console.error(e) }
         } catch (e) {
             if (e?.name == "AxiosError" && e?.response?.status) {
                 await interaction.editReply({
@@ -78,5 +67,5 @@ module.exports = {
                 throw e;
             }
         }
-    },
-};
+    }
+}
