b0e8f6d91f14037c6e45fa6b38819af309e9e117
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-30 17:59:58 +0900

    :-1:

--- discordbot.js
+++ discordbot.js
@@ -1,7 +1,7 @@
 // Discord.js Bot - by ringoXD
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '1';
 const { Client, Events, GatewayIntentBits, Status, ActivityType } = require('discord.js');
-const { Guilds, VoiceState } = GatewayIntentBits
+const { Guilds } = GatewayIntentBits
 const fs = require("fs");
 const path = require("path");
 const { token, linkPort, linkDomain, guildId } = require('./config.json');
@@ -33,7 +33,7 @@ fs.readdirSync(path.join(__dirname, "commands"), {
 })
 
 const options = {
-	intents: [Guilds, VoiceState]
+	intents: [Guilds]
 };
 
 const client = new Client(options);
