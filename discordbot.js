b29750bb4f77747d8dea66c61f15cfeb949767c8
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-30 17:44:52 +0900

    intents

--- discordbot.js
+++ discordbot.js
@@ -1,6 +1,6 @@
 // Discord.js Bot - by ringoXD
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '1';
-const { Client, Events, Intents, Status, ActivityType } = require('discord.js');
+const { Client, Events, GatewayIntentsBits, Status, ActivityType } = require('discord.js');
 const fs = require("fs");
 const path = require("path");
 const { token, linkPort, linkDomain, guildId } = require('./config.json');
@@ -32,7 +32,7 @@ fs.readdirSync(path.join(__dirname, "commands"), {
 })
 
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES]
+	intents: [GatewayIntentsBits.Guilds, GatewayIntentsBits.GUILD_VOICE_STATES]
 })
 
 // this is the entrypoint for discord-player based application
