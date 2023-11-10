ca5e0eb8769b46df75dc71a764725e4f8282063e
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-11-10 21:02:00 +0900

    use rcon

--- commands/admintool.js
+++ commands/admintool.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder, IntegrationApplication } = require('discord.js');
-const { AdminRoleID } = require('../config.json');
+const { Rcon } = require('rcon-client');
+
+const { AdminRoleID, rconhost1, rconport1, rconpass1 } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -27,6 +29,17 @@ module.exports = {
 		if (interaction.options.getSubcommand() === 'mc_restart') {
 			if (interaction.options.getString('target') ==='main') {
                 await interaction.reply('<a:loading:1071641234310955129> 接続中...')
+				console.log(`Connecting to ${rconhost1}:${rconport1}...`)
+				const rcon = new Rcon({
+					host: rconhost1,
+                    port: rconport1,
+                    password: rconpass1
+				});
+				rcon.connect();
+				rcon.send('/say はろーわーるど');
+				rcon.send('/discord bcast はろーわーるど!');
+				rcon.end();
+				interaction.editReply(`<:check:962405846002847754> サーバーにリクエストを送信しました!`)
             } else if (interaction.options.getString('target') === 'creative') {
                 await interaction.reply('準備中')
             } else if (interaction.options.getString('target') === 'pvp') {
