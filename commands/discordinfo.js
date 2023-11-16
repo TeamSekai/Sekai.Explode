fdf3d0eed99c71510d37d9f3a9b9199429b443ab
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-11-16 22:57:35 +0900

    serverinfo test

--- commands/discordinfo.js
+++ commands/discordinfo.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -21,6 +21,28 @@ module.exports = {
 				.setDescription('サーバーの情報を調べます!')
 		),
 		execute: async function (interaction) {
-			await interaction.reply("Soon...")
+			const subcommand = interaction.options.getSubcommand();
+			if (subcommand === 'user') {
+				await interaction.reply("さーせん、こいつまだ作ってないんすわ")
+			}
+			if (subcommand === 'server') {
+				const guild = interaction.guild;
+				const gmembers = guild.memberCount;
+				const gchannels = guild.channels.cache.size;
+				const gvoicechannels = guild.channels.cache.filter(channel => channel.type === ChannelType.GuildVoice).size;
+				const groles = guild.roles.cache.size;
+				const boostStatus = guild.premiumSubscriptionCount > 0 ? `ブースト中（ブースト数: ${guild.premiumSubscriptionCount}）` : 'ブーストなし';
+				const createdAt = guild.createdAt.toDateString();
+
+				const serverInfoMessage = `サーバーの情報\n
+					人数: ${gmembers}\n
+					テキストチャンネル数: ${gchannels}\n
+					ボイスチャンネル数: ${gvoicechannels}\n
+					ロール数: ${groles}\n
+					作成日: ${createdAt}\n
+					ブースト状態: ${boostStatus}`;
+
+				await interaction.reply(serverInfoMessage);
+			}
 		}
 };
