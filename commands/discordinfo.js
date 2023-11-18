baba041be53e298cd25c4f272dd3c50eaa35710e
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-11-18 22:12:51 +0900

    embeds!

--- commands/discordinfo.js
+++ commands/discordinfo.js
@@ -27,32 +27,49 @@ module.exports = {
 				const member = await interaction.guild.members.fetch(user.id)
 				const username = user.username
 				const userid = user.id
-				const createdAt = `<t:${Math.floor(user.createdTimestamp / 1000)}:f>`; //*ChatGPT MOMENT
-				const joinDate = `<t:${Math.floor(member.joinedTimestamp / 1000)}:f>`;
+				const createdAt = `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`; //*ChatGPT MOMENT
+				const joinDate = `<t:${Math.floor(member.joinedTimestamp / 1000)}:R>`;
 				const roles = member.roles.cache
 					.filter(role => role.id !== interaction.guild.roles.everyone.id)
 					.map(role => role.name)
 					.join(', ');
+				const rolecount = roles.size
 
-				const userInfoMessage = `ユーザーの情報\n
-                名前: ${username} (id: ${userid})\n
-                アカウント作成日: ${createdAt}\n
-                サーバー参加日: ${joinDate}\n
-                所持しているロール: ${roles}`;
-
-				await interaction.reply(userInfoMessage);
+				await interaction.reply({
+					embeds: [{
+						title: `${username}の情報`,
+						color: 0x77e4a6,
+						fields: [{
+							name: "ユーザーID",
+							value: userid
+						}, {
+							name: "アカウント作成日",
+							value: createdAt,
+							inline: true
+						}, {
+							name: "サーバー参加日",
+							value: joinDate,
+							inline: true
+						}, {
+							name: `ロール(${role.size})`,
+							value: roles
+						}]
+					}]
+				})
 
 			}
 			if (subcommand === 'server') {
 				const guild = interaction.guild;
+				const guildname = guild.name
 				const gmembers = guild.memberCount;
 				const gchannels = guild.channels.cache.size;
 				const gvoicechannels = guild.channels.cache.filter(channel => channel.type === ChannelType.GuildVoice).size;
 				const groles = guild.roles.cache.size;
-				const boostStatus = guild.premiumSubscriptionCount > 0 ? `ブースト中（${guild.premiumSubscriptionCount} Boost!）` : 'ブーストなし';
-				const createdAt = `<t:${Math.floor(guild.createdTimestamp / 1000)}:f>`;
+				const boostStatus = guild.premiumSubscriptionCount > 0 ? `あり(${guild.premiumSubscriptionCount} ブースト）` : 'なし';
+				const createdAt = `<t:${Math.floor(guild.createdTimestamp / 1000)}:R>`;
 				console.log(`${guild.createdAt} - ${createdAt}`)
 
+				/*
 				const serverInfoMessage = `サーバーの情報\n
 					人数: ${gmembers}\n
 					テキストチャンネル数: ${gchannels}\n
@@ -60,8 +77,32 @@ module.exports = {
 					ロール数: ${groles}\n
 					作成日: ${createdAt}\n
 					ブースト状態: ${boostStatus}`;
+				*/
 
-				await interaction.reply(serverInfoMessage);
+				await interaction.reply({
+					embeds: [{
+						title: `${guildname}の情報`,
+						color: 0x52c9e0,
+						fields: [{
+							name: "サーバー人数",
+							value: gmembers,
+							inline: true
+						}, {
+							name: "サーバー作成日",
+							value: createdAt,
+							inline: true
+						}, {
+							name: "チャンネル数",
+							value: `テキストチャンネル: ${gchannels}\n` + `ボイスチャンネル: ${gvoicechannels}`
+						}, {
+							name: "ロール数",
+							value: groles
+						}, {
+							name: "ブーストの有無",
+							value: boostStatus
+						}]
+					}]
+				})
 			}
 		}
 };
