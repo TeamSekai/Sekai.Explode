728c619c0db98bc5e94b065d29ad498fb1bd92ba
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-12-14 00:10:23 +0900

    idk

--- commands/pie_chart.js
+++ commands/pie_chart.js
@@ -1,3 +1,4 @@
+/*
 const { SlashCommandBuilder } = require('discord.js');
 const { ChartJSNodeCanvas } = require("chartjs-node-canvas")
 const { createCanvas, loadImage } = require('canvas');
@@ -88,3 +89,4 @@ module.exports = {
 };
 
 
+*/
