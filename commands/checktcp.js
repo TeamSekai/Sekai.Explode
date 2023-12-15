33247c989849f3ba11bab8fccf9b286de11c3162
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-12-15 22:51:50 +0900

    fuck

--- commands/checktcp.js
+++ commands/checktcp.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('dev02')
+        .setName('dev03')
         .setDescription('view'),
     execute: async function (interaction) {
         let url = args[0];
