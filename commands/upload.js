41bd5ee9da2bba4197255ef06844acfdfad2de4f
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-12-26 21:45:11 +0900

    typo

--- commands/upload.js
+++ commands/upload.js
@@ -56,7 +56,7 @@ module.exports = {
 			console.log(res)
 			console.log("==========")
 			console.log(res2)
-            interaction.editReply((isPrivate ? " /private/" : "") + `/${res2.data.fileName} としてアップロードしました！`);
+            interaction.editReply((isPrivate ? " /private/" : "") + "${res2.data.fileName} としてアップロードしました！`);
             const user = interaction.user;
             const dmChannel = await user.createDM();
             dmChannel.send({
