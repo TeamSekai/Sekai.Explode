8cf83e8eed50a2afd9568fe74c2c47722e197dad
ringo360 <105296365+ringo360@users.noreply.github.com>
2024-01-02 00:54:20 +0900

    let's make global ban system

--- commands/globalban.js
+++ commands/globalban.js
@@ -2,8 +2,24 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('dev02')
-        .setDescription('view'),
+        .setName('gban')
+        .setDescription('作成中')
+		.addSubcommand(subcommand => 
+			subcommand
+				.setName('sync')
+				.setDescription('データベースとの同期を行います。(BANされていないユーザーが居た場合には自動BANを行います。')
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('add')
+				.setDescription('グローバルBANリストにユーザーを追加します.')
+				.addUserOption(option => (
+					option
+						.setName("user")
+						.setDescription("ユーザーを指定します。")
+						.setRequired(true)
+				))
+		),
     execute: async function (interaction) {
         await interaction.reply(`ふぁっきゅー`)
     }
