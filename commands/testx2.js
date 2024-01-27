1207256bea50105ac795f56d138fb3c0c78092f2
ringo360 <105296365+ringo360@users.noreply.github.com>
2024-01-27 16:28:16 +0900

    uwu

---
+++ commands/testx2.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('idk')
+        .setDescription('shitcord code test'),
+    execute: async function (interaction) {
+		/** @type {import("discord.js").CommandInteraction} */
+        const usr = interaction.user.id
+		const member = await interaction.guild.members.fetch(usr)
+		const roles = mmeber.roles.cache
+		const sortedRoles = roles.sort((a, b) => b.position - a.position);
+		const topRole = sortedRoles.first();
+		await interaction.reply(`Result: ${topRole}`)
+    }
+};
