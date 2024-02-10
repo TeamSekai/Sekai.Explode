86bc3ff556ec02c47d216d8f58796edf1579ff51
takejohn <105504345+takejohn@users.noreply.github.com>
2024-02-11 00:20:15 +0900

    /queue が Pager を使用するように変更

--- commands/queue.js
+++ commands/queue.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 const { useQueue } = require('discord-player');
+const Pager = require('../util/pager');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -23,82 +24,16 @@ module.exports = {
         const tracks = queuedTracks.map((track, idx) => `**${idx + 1})** [${track.title}](${track.url})`);
 
         const chunkSize = 10;
-        const pages = Math.ceil(tracks.length / chunkSize);
 
-        let currentPage = 0;
-
-        const embed = new EmbedBuilder()
-            .setColor('Red')
-            .setTitle(`${queue.tracks.size}件の曲がキューに入っています!`)
-            .setDescription(
-                tracks.slice(currentPage * chunkSize, (currentPage + 1) * chunkSize).join('\n') || '**キューに曲がありません！**',
-            )
-            .setFooter({
-                text: `${currentPage + 1}ページ目 | ${queue.tracks.size}曲`,
-            });
-
-        if (pages === 1) {
-            return interaction.reply({
-                embeds: [embed],
-            });
-        }
-
-        const prevButton = new ButtonBuilder()
-            .setCustomId('prev')
-            .setLabel('前のページ')
-            .setStyle(ButtonStyle.Secondary)
-            .setEmoji('⬅️');
-
-        const nextButton = new ButtonBuilder()
-            .setCustomId('next')
-            .setLabel('次のページ')
-            .setStyle(ButtonStyle.Secondary)
-            .setEmoji('➡️');
-
-        const row = new ActionRowBuilder().addComponents(prevButton, nextButton);
-
-        const message = await interaction.reply({
-            embeds: [embed],
-            components: [row],
-            fetchReply: true,
-        });
-
-        const collector = message.createMessageComponentCollector({
-            idle: 60000,
-        });
-
-        collector.on('collect', i => {
-            i.deferUpdate();
-
-            switch (i.customId) {
-                case 'prev':
-                    currentPage = currentPage === 0 ? pages - 1 : currentPage - 1;
-                    break;
-                case 'next':
-                    currentPage = currentPage === pages - 1 ? 0 : currentPage + 1;
-                    break;
-                default:
-                    break;
-            }
-
-            embed
-                .setDescription(
-                    tracks.slice(currentPage * chunkSize, (currentPage + 1) * chunkSize).join('\n') || '**キューに曲がありません！**',
-                )
-                .setFooter({
-                    text: `${currentPage + 1}ページ目 | ${queue.tracks.size}曲`,
-                });
-
-            message.edit({
-                embeds: [embed],
-                components: [row],
-            });
-        });
-
-        collector.on('end', () => {
-            message.edit({
-                components: [],
-            });
-        });
+        const pager = new Pager(tracks, {
+            pageLength: chunkSize,
+            color: 'Red',
+            title: `${queue.tracks.size}件の曲がキューに入っています!`,
+            emptyMessage: '**キューに曲がありません！**',
+            footer: pager => ({  // 丸括弧を付けないとブロックとして解釈されてしまうという罠
+                text: `${pager.page + 1}ページ目 | ${queue.tracks.size}曲`
+            })
+        })
+        pager.replyTo(interaction);
     },
 };
