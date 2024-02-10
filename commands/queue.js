9facf423a68773ad5f78f78df9d1c46c2b9d26b6
takejohn <105504345+takejohn@users.noreply.github.com>
2024-02-10 21:21:20 +0900

    インデントをスペース4個に統一

--- commands/queue.js
+++ commands/queue.js
@@ -8,97 +8,97 @@ module.exports = {
     execute: async function (interaction) {
         const queue = useQueue(interaction.guildId);
 
-    if (!interaction.member.voice.channelId)
-      return await interaction.reply({ content: 'えー実行したくないなぁー...だってVCに君が居ないんだもん...', ephemeral: true });
-    if (
-      interaction.guild.members.me.voice.channelId &&
-      interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId
-    )
-      return await interaction.reply({ content: 'えー実行したくないなぁー...だってVCに君が居ないんだもん...', ephemeral: true });
-
-	if (!queue || !queue.isPlaying())
-	  return interaction.reply({ content: `再生されている曲がありません！`, ephemeral: true });
-
-	const queuedTracks = queue.tracks.toArray();
-    const tracks = queuedTracks.map((track, idx) => `**${idx + 1})** [${track.title}](${track.url})`);
-
-    const chunkSize = 10;
-    const pages = Math.ceil(tracks.length / chunkSize);
-
-    let currentPage = 0;
-
-    const embed = new EmbedBuilder()
-      .setColor('Red')
-      .setTitle(`${queue.tracks.size}件の曲がキューに入っています!`)
-      .setDescription(
-        tracks.slice(currentPage * chunkSize, (currentPage + 1) * chunkSize).join('\n') || '**キューに曲がありません！**',
-      )
-      .setFooter({
-        text: `${currentPage + 1}ページ目 | ${queue.tracks.size}曲`,
-      });
-
-    if (pages === 1) {
-      return interaction.reply({
-        embeds: [embed],
-      });
-    }
-
-    const prevButton = new ButtonBuilder()
-      .setCustomId('prev')
-      .setLabel('前のページ')
-      .setStyle(ButtonStyle.Secondary)
-      .setEmoji('⬅️');
-
-    const nextButton = new ButtonBuilder()
-      .setCustomId('next')
-      .setLabel('次のページ')
-      .setStyle(ButtonStyle.Secondary)
-      .setEmoji('➡️');
-
-    const row = new ActionRowBuilder().addComponents(prevButton, nextButton);
-
-    const message = await interaction.reply({
-      embeds: [embed],
-      components: [row],
-      fetchReply: true,
-    });
-
-    const collector = message.createMessageComponentCollector({
-      idle: 60000,
-    });
-
-    collector.on('collect', i => {
-      i.deferUpdate();
-
-      switch (i.customId) {
-        case 'prev':
-          currentPage = currentPage === 0 ? pages - 1 : currentPage - 1;
-          break;
-        case 'next':
-          currentPage = currentPage === pages - 1 ? 0 : currentPage + 1;
-          break;
-        default:
-          break;
-      }
-
-      embed
-        .setDescription(
-          tracks.slice(currentPage * chunkSize, (currentPage + 1) * chunkSize).join('\n') || '**キューに曲がありません！**',
+        if (!interaction.member.voice.channelId)
+            return await interaction.reply({ content: 'えー実行したくないなぁー...だってVCに君が居ないんだもん...', ephemeral: true });
+        if (
+            interaction.guild.members.me.voice.channelId &&
+            interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId
         )
-        .setFooter({
-          text: `${currentPage + 1}ページ目 | ${queue.tracks.size}曲`,
+            return await interaction.reply({ content: 'えー実行したくないなぁー...だってVCに君が居ないんだもん...', ephemeral: true });
+
+        if (!queue || !queue.isPlaying())
+            return interaction.reply({ content: `再生されている曲がありません！`, ephemeral: true });
+
+        const queuedTracks = queue.tracks.toArray();
+        const tracks = queuedTracks.map((track, idx) => `**${idx + 1})** [${track.title}](${track.url})`);
+
+        const chunkSize = 10;
+        const pages = Math.ceil(tracks.length / chunkSize);
+
+        let currentPage = 0;
+
+        const embed = new EmbedBuilder()
+            .setColor('Red')
+            .setTitle(`${queue.tracks.size}件の曲がキューに入っています!`)
+            .setDescription(
+                tracks.slice(currentPage * chunkSize, (currentPage + 1) * chunkSize).join('\n') || '**キューに曲がありません！**',
+            )
+            .setFooter({
+                text: `${currentPage + 1}ページ目 | ${queue.tracks.size}曲`,
+            });
+
+        if (pages === 1) {
+            return interaction.reply({
+                embeds: [embed],
+            });
+        }
+
+        const prevButton = new ButtonBuilder()
+            .setCustomId('prev')
+            .setLabel('前のページ')
+            .setStyle(ButtonStyle.Secondary)
+            .setEmoji('⬅️');
+
+        const nextButton = new ButtonBuilder()
+            .setCustomId('next')
+            .setLabel('次のページ')
+            .setStyle(ButtonStyle.Secondary)
+            .setEmoji('➡️');
+
+        const row = new ActionRowBuilder().addComponents(prevButton, nextButton);
+
+        const message = await interaction.reply({
+            embeds: [embed],
+            components: [row],
+            fetchReply: true,
         });
 
-      message.edit({
-        embeds: [embed],
-        components: [row],
-      });
-    });
+        const collector = message.createMessageComponentCollector({
+            idle: 60000,
+        });
 
-    collector.on('end', () => {
-      message.edit({
-        components: [],
-      });
-    });
-  },
+        collector.on('collect', i => {
+            i.deferUpdate();
+
+            switch (i.customId) {
+                case 'prev':
+                    currentPage = currentPage === 0 ? pages - 1 : currentPage - 1;
+                    break;
+                case 'next':
+                    currentPage = currentPage === pages - 1 ? 0 : currentPage + 1;
+                    break;
+                default:
+                    break;
+            }
+
+            embed
+                .setDescription(
+                    tracks.slice(currentPage * chunkSize, (currentPage + 1) * chunkSize).join('\n') || '**キューに曲がありません！**',
+                )
+                .setFooter({
+                    text: `${currentPage + 1}ページ目 | ${queue.tracks.size}曲`,
+                });
+
+            message.edit({
+                embeds: [embed],
+                components: [row],
+            });
+        });
+
+        collector.on('end', () => {
+            message.edit({
+                components: [],
+            });
+        });
+    },
 };
