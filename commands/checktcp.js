d8863a62093540f2ba3b791ad98186a55c95cc6b
takejohn <105504345+takejohn@users.noreply.github.com>
2024-02-22 16:44:39 +0900

    最後の1回のみ編集を行う

--- commands/checktcp.js
+++ commands/checktcp.js
@@ -28,21 +28,24 @@ module.exports = {
         let checkResult = async () => {
             checkCount++;
             let res2 = await axios.get("https://check-host.net/check-result/" + res.data.request_id)
-            if (checkCount < 8 && (Object.values(res2.data).filter(x => x?.length != 0)).length < (res.data.nodes.length * 0.8)) setTimeout(checkResult, 2000);
-            let str = Object.entries(res2.data).map(([key, value]) => {
-                let nodeName = key.replace(".node.check-host.net", "");
-                let data = value?.[0];
-                console.log(strFormat(LANG.common.message.dataFor, [nodeName]), data);
-                if (!value || !data) return `[${nodeName}] Timeout`;
-                return `[${nodeName}] ${data[3] || "Error"}/${data[2]} | Ping: ${Math.floor(data[1] * 1000)}ms`;
-            }).filter(x => !!x).join("\n");
-            msg.edit({
-                content: LANG.common.message.result,
-                files: [{
-                    attachment: Buffer.from(str),
-                    name: "result.txt"
-                }]
-            })
+            if (checkCount < 8 && (Object.values(res2.data).filter(x => x?.length != 0)).length < (res.data.nodes.length * 0.8)) {
+                setTimeout(checkResult, 2000);
+            } else {
+                let str = Object.entries(res2.data).map(([key, value]) => {
+                    let nodeName = key.replace(".node.check-host.net", "");
+                    let data = value?.[0];
+                    console.log(strFormat(LANG.common.message.dataFor, [nodeName]), data);
+                    if (!value || !data) return `[${nodeName}] Timeout`;
+                    return `[${nodeName}] ${data[3] || "Error"}/${data[2]} | Ping: ${Math.floor(data[1] * 1000)}ms`;
+                }).filter(x => !!x).join("\n");
+                msg.edit({
+                    content: LANG.common.message.result,
+                    files: [{
+                        attachment: Buffer.from(str),
+                        name: "result.txt"
+                    }]
+                });
+            }
         };
         setTimeout(checkResult, 2000);
     }
