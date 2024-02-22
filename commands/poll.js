a878d5ef5643a5883a40f19299c46962abfc721c
ringoXD <nep360i@gmail.com>
2024-02-22 16:31:35 +0900

    uwu

--- commands/poll.js
+++ commands/poll.js
@@ -2,10 +2,25 @@ const { SlashCommandBuilder } = require('discord.js');
 const { LANG } = require('../util/languages');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName(LANG.commands.poll.name)
-        .setDescription(LANG.commands.poll.description),
-    execute: async function (interaction) {
-        // await interaction.reply(LANG.commands.poll.message)
-    }
+	data: new SlashCommandBuilder()
+		.setName(LANG.commands.poll.name)
+		.setDescription(LANG.commands.poll.description)
+		.addStringOption(option=>option.setName('title').setDescription('Pollのタイトル').setRequired(true))
+		.addStringOption(option=>option.setName('choice1').setDescription('選択肢を入力...').setRequired(true))
+		.addStringOption(option=>option.setName('choice2').setDescription('選択肢を入力...').setRequired(true))
+		.addStringOption(option=>option.setName('choice3').setDescription('選択肢を入力...'))
+		.addStringOption(option=>option.setName('choice4').setDescription('選択肢を入力...'))
+		.addStringOption(option=>option.setName('choice5').setDescription('選択肢を入力...'))
+		.addStringOption(option=>option.setName('choice6').setDescription('選択肢を入力...'))
+		.addStringOption(option=>option.setName('choice7').setDescription('選択肢を入力...'))
+		.addStringOption(option=>option.setName('choice8').setDescription('選択肢を入力...'))
+		.addStringOption(option=>option.setName('choice9').setDescription('選択肢を入力...'))
+		.addStringOption(option=>option.setName('choice10').setDescription('選択肢を入力...')),
+	execute: async function (interaction) {
+		await interaction.deferReply();
+		const { channel } = await interaction;
+		const options = await interaction.options.data();
+		await interaction.editReply('soon')
+		// await interaction.reply(LANG.commands.poll.message)
+	}
 };
