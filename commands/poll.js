01d536105fc4082755c79ad1f0fd1e664180a3f7
ringoXD <nep360i@gmail.com>
2024-02-25 01:18:58 +0900

    pollを多言語対応化させた

--- commands/poll.js
+++ commands/poll.js
@@ -3,20 +3,19 @@ const { LANG } = require('../util/languages');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		//TODO: i18n support
-		.setName('poll')
-		.setDescription('soon')
-		.addStringOption(option=>option.setName('title').setDescription('Pollのタイトル').setRequired(true))
-		.addStringOption(option=>option.setName('choice1').setDescription('選択肢を入力...').setRequired(true))
-		.addStringOption(option=>option.setName('choice2').setDescription('選択肢を入力...').setRequired(true))
-		.addStringOption(option=>option.setName('choice3').setDescription('選択肢を入力...'))
-		.addStringOption(option=>option.setName('choice4').setDescription('選択肢を入力...'))
-		.addStringOption(option=>option.setName('choice5').setDescription('選択肢を入力...'))
-		.addStringOption(option=>option.setName('choice6').setDescription('選択肢を入力...'))
-		.addStringOption(option=>option.setName('choice7').setDescription('選択肢を入力...'))
-		.addStringOption(option=>option.setName('choice8').setDescription('選択肢を入力...'))
-		.addStringOption(option=>option.setName('choice9').setDescription('選択肢を入力...'))
-		.addStringOption(option=>option.setName('choice10').setDescription('選択肢を入力...')),
+		.setName(LANG.commands.poll.name)
+		.setDescription(LANG.commands.poll.description)
+		.addStringOption(option=>option.setName('title').setDescription(LANG.commands.poll.polltitle).setRequired(true))
+		.addStringOption(option=>option.setName('choice1').setDescription(LANG.commands.poll.pollchoices).setRequired(true))
+		.addStringOption(option=>option.setName('choice2').setDescription(LANG.commands.poll.pollchoices).setRequired(true))
+		.addStringOption(option=>option.setName('choice3').setDescription(LANG.commands.poll.pollchoices))
+		.addStringOption(option=>option.setName('choice4').setDescription(LANG.commands.poll.pollchoices))
+		.addStringOption(option=>option.setName('choice5').setDescription(LANG.commands.poll.pollchoices))
+		.addStringOption(option=>option.setName('choice6').setDescription(LANG.commands.poll.pollchoices))
+		.addStringOption(option=>option.setName('choice7').setDescription(LANG.commands.poll.pollchoices))
+		.addStringOption(option=>option.setName('choice8').setDescription(LANG.commands.poll.pollchoices))
+		.addStringOption(option=>option.setName('choice9').setDescription(LANG.commands.poll.pollchoices))
+		.addStringOption(option=>option.setName('choice10').setDescription(LANG.commands.poll.pollchoices)),
 	execute: async function (interaction) {
 		await interaction.deferReply();
 		const { channel } = await interaction;
