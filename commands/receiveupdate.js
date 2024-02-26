04801baf2e4d0782cf75f3393e30d17bece8d4b7
ringoXD <nep360i@gmail.com>
2024-02-27 00:26:36 +0900

    okay...

--- commands/receiveupdate.js
+++ commands/receiveupdate.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const config = require('../config.json')
 const { LANG } = require('../util/languages');
 
-// いいかんじに
 module.exports = {
     data: new SlashCommandBuilder()
+		//TODO: i18n
         .setName('follow_announcements')
         .setDescription('Sekai.Explode開発者からのアナウンスを受信します!')
 		.addChannelOption(option => option
@@ -14,7 +15,7 @@ module.exports = {
 		),
     execute: async function (interaction, client) {
 		const targetchannel = interaction.options.getChannel('channel');
-		client.channels.resolve("1211685593025609749").addFollower(targetchannel.id)
+		client.channels.resolve("1211685593025609749").addFollower(targetchannel.id) //TODO: config.jsonで編集可能に?
 			.then(() =>
 				console.log(`[Sekai.Explode] new follower! ${interaction.guild.name} - ${interaction.guild.id}`),
 				interaction.reply(`<#${targetchannel.id}>にSekai.Explodeのアナウンスを通知します :wave:`)
