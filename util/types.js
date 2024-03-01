87a7f8ecbe3c0058a3abd6f7a336c62b669b5bdd
takejohn <105504345+takejohn@users.noreply.github.com>
2024-03-02 01:10:08 +0900

    util/typesをモジュールとして認識させる

--- util/types.js
+++ util/types.js
@@ -7,3 +7,5 @@
  * @property {import("discord.js").SlashCommandSubcommandsOnlyBuilder} data Discord の API に登録するデータ
  * @property {(interaction: import("discord.js").ChatInputCommandInteraction) => Promise<void>} execute コマンドの処理
  */
+
+module.exports = {};
